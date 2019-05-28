b4174f83d6b22e657e44a21e940bba51db86533b
Rafał Borkowski <rafal.Borkowski@newvoicemedia.com>
2019-05-28 14:59:30 +0200

    Controller changes

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -6,23 +6,69 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
+
+    public enum WriteStatus
+    {
+        Written=56,
+        Deleted=78,
+        Empty=1000
+    }
+
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+
+
+        private List<string> lista;
+
+        public ValuesController()
         {
-            return new string[] { "value1", "value2" };
+            lista = new List<string>();
+            lista.Add("aaaaa");
+            lista.Add("bbbbb");
+            lista.Add("ccccc");
+        }
+
+        // GET api/values
+        [HttpGet("sum,{a},{b}")]
+        public int Get(int a, int b)
+        {
+            return a+b;
+        }
+        //1. /sum (a,b)=> a+b
+        //2. echo a => echo a (string)
+        //3. a,b,c => List(a,b,c)
+        //4. 0..4(enum) => text
+
+        [HttpGet("text,{text}")]
+        public string Getsdfsdfds(string text)
+        {
+            return "echo "+text;
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("list,{a},{b},{c}")]
+        public List<string> Get(string a, string b, string c)
         {
-            return "value";
+            lista.Add(a);
+            lista.Add(b);
+            lista.Add(c);
+            return lista;
         }
 
+        [HttpGet("enum,{enumName}")]
+        public int Getaaa(string enumName)
+        {
+            WriteStatus writeStatus= Enum.Parse<WriteStatus>(enumName);
+            return (int)writeStatus;
+        }
+
+        [HttpGet("enum2,{enumNumber}")]
+        public string Getaaa(int enumNumber)
+        {
+            WriteStatus writeStatus =(WriteStatus) enumNumber;
+            return writeStatus.ToString();
+        }
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
